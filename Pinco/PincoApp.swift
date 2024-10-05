98625d0c17aefb6bb3d27c96ae29b4212401f93a
keshew <artyom.korotckov2017@yandex.ru>
2024-10-05 12:54:23 +0500

    Initial Commit

---
+++ Pinco/PincoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PincoApp.swift
+//  Pinco
+//
+//  Created by Артём Коротков on 05.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct PincoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
