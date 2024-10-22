5fdefa43added03394210bdb3a175c753949973b
keshew <artyom.korotckov2017@yandex.ru>
2024-10-22 17:43:52 +0500

    fix bug

---
+++ Pinco/View/PauseView.swift
@@ -0,0 +1,18 @@
+//
+//  PauseView.swift
+//  Pinco
+//
+//  Created by Артём Коротков on 22.10.2024.
+//
+
+import SwiftUI
+
+struct PauseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PauseView()
+}
