827177bf5f6cde886cd9fa9b43e94986a2ecd6a0
keshew <artyom.korotckov2017@yandex.ru>
2024-10-23 23:13:04 +0500

    adaptive done; work on data base

---
+++ Pinco/View/VictoryView.swift
@@ -0,0 +1,18 @@
+//
+//  VictoryView.swift
+//  Pinco
+//
+//  Created by Артём Коротков on 23.10.2024.
+//
+
+import SwiftUI
+
+struct VictoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VictoryView()
+}
